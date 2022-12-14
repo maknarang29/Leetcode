b2fb140a1fd0720605cfe7348ae62e7feb869fc4
maknarang29 <maknarang29@gmail.com>
2022-12-14 22:16:58 +0530

    LC problem of day

--- 931 Minimum Falling Path Sum/minSum.cpp
+++ 931 Minimum Falling Path Sum/minSum.cpp
@@ -1,5 +1,3 @@
-#include <bits/stdc++.h>
-#include<vector>
 #include<iterator>
 #include<iostream>
 using namespace std;
@@ -15,13 +13,21 @@ using namespace std;
         int min;
         vector<int>::iterator st = vec.begin();
         vector<int>::iterator nd = vec.begin();
-        if (ind == -1)  return *min_element(vec.begin(),vec.end());
+        if (ind == -1 )  return *min_element(vec.begin(),vec.end());
+        else if(ind == 0 ){
+            advance(nd,2);
+            return *min_element(st,nd);
+            
+        }if (ind == -1)  return *min_element(vec.begin(),vec.end());
         else if(ind == 0){
+            if (vec.size()==2) return *min_element(vec.begin(),vec.end()); 
+            else{
             advance(nd,1);
-            return *min_element(st,nd); 
+            return *min_element(st,nd);
+            } 
         }
         else if(ind == vec.size()-1 ){
-           advance(st,vec.size()-2);
+           advance(st,vec.size()-1);
            return *min_element(st,nd); 
         }
         else { 
@@ -33,15 +39,18 @@ using namespace std;
     int minFallingPathSum(vector<vector< int > > &vec){
         vector<int> minnums;
         vector<int> indexes;
+        int val = 0;
         int min = minVal(vec[0],-1);
+        val += min; 
         int ind = getIndex(vec[0],min);
         minnums.push_back(min);
         indexes.push_back(ind);
         for (int i = 1; i<vec.size();i++){
+            val+= minVal(vec[i],indexes[indexes.size()-1]);
             minnums.push_back(minVal(vec[i],indexes[indexes.size()-1]));
             indexes.push_back(getIndex(vec[i],minnums[minnums.size()-1]));
         }
-        return accumulate(minnums.begin(),minnums.end(),0);
+        return val; 
     }
 
 
@@ -60,12 +69,21 @@ int main(){
     
         vec.push_back(v1);
     }
-    for (int i = 0; i < vec.size(); i++) {
-        for (int j = 0; j < vec[i].size(); j++)
-            cout << vec[i][j] << " ";
+    // for (int i = 0; i < vec.size(); i++) {
+    //     for (int j = 0; j < vec[i].size(); j++)
+    //         cout << vec[i][j] << " ";
+    //     cout << endl;
+    // }
+    vector<int> v1 {82,81};
+    vector<int> v2 {69,33};
+    vector<vector<int>> v;
+    v.push_back(v1);
+    v.push_back(v2);
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v[i].size(); j++)
+            cout << v[i][j] << " ";
         cout << endl;
     }
-    
-    cout<<minFallingPathSum(vec); 
-    
+    //cout<<minVal(v[1],0)<<endl; 
+    cout<<minFallingPathSum(v);
 }
