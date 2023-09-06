bade6344420eb1b9af3dc987cfc263a06aa52a2b
maknarang29 <maknarang29@gmail.com>
2023-09-06 23:07:29 +0530

    working

--- 725 Split LinkedList/splitLL.cpp
+++ 725 Split LinkedList/splitLL.cpp
@@ -5,32 +5,31 @@
 #include "LLFuncs.cpp"
 
 
-   vector<Node*> splitListToParts(Node* head, int k) {
-        vector<Node*> vec(k);
-        int size = 0;
-        Node* temp = head;
-        while(temp){
-            temp=temp->next;
-            size++;
+vector<Node*> splitListToParts(Node* head, int k) {
+    vector<Node*> vec(k,NULL);
+    int size = 0;
+    for (Node* node = head; node; node = node->next)    size++;
+    int step = size/k, extra = size%k ;
+    Node* node = head, *prev = NULL;
+    for (int i = 0;node && i<k;i++,extra--){
+        vec[i] = node;
+        for(int j =0; j < step + ( extra > 0); j ++){
+            prev = node;
+            node = node->next;
         }
-        int len = floor(size/k);
-        for (int i = 0;i<k;i++){
-            if (i%len == 0){
-                vec.push_back(head);
-                head=head->next;
-            }
-        }
-        return vec;
-        
+        prev->next = NULL; 
     }
-
+    return vec;
+}
 
 int main(){
 
     Node* head = new Node();
     int lb = 0, ub = 10;
     for (int i = 0;i<11;i++) addTail(head, rand() % (ub - lb + 1));
-    vector<Node*> ans = splitListToParts(head,2);
-    traversalLoop(ans[0]); 
+    traversalLoop(head);
     cout<<endl;
+    vector<Node*> ans = splitListToParts(head,2);
+    for (Node* i : ans) {traversalLoop(i);cout<<endl;} 
+    
 }
